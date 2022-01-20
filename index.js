e4f1a74611377f75d34943b8dd0beb2d60a33cc8
I7RANK <franfgh2000@hotmail.com>
2022-01-19 23:08:51 -0500

    Adding lobby event and creating sender room

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const res = require('express/lib/response');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -21,13 +22,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
 
   const path = [];
 
   socket.on('test-location', (client) => {
     path.push({lat: client.lat, lng: client.lng});
-    console.log(path);
   });
 
   socket.on('disconnect', () => {
@@ -40,6 +40,19 @@ io.on('connection', (socket) => {
 
     let data = JSON.stringify(trackingHistory);
     fs.writeFileSync('bike_tracking.json', data);
+  });
+
+  socket.on('lobby', (clientObj) => {
+    if (clientObj.type === 'sender') {
+      console.log('im the sender', clientObj.socketId);
+      const roomId = `${clientObj.socketId}-${uuidv4()}`;
+
+      clientObj['roomId'] = roomId;
+
+      socket.join(roomId);
+
+      io.to(clientObj.socketId).emit('private message', clientObj);
+    }
   });
 });
 
