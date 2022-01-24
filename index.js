0292a4ee1e2587ea7e427002012f382462eb2884
I7RANK <franfgh2000@hotmail.com>
2022-01-24 18:26:26 -0500

    CORS accept all connections

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { getPathRoadsAPI } = require('./utils/roadsAPI/getPathRoadsAPI');
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET"]
   }
 });;
